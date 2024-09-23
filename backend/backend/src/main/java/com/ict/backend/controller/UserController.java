69b36bca4e1e40af4795919adb494293f98ef49d
RnfElwl <cherryhey2004@gmail.com>
2024-09-23 14:52:12 +0900

    font및 back 기본 구조

---
+++ backend/backend/src/main/java/com/ict/backend/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.ict.backend.controller;
+
+import com.ict.backend.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+@CrossOrigin(origins =  "*")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/view")
+    public String view() {
+        log.info("정보 들어옴");
+        return "hi";
+    }
+
+
+}
---
+++ backend/backend/src/main/java/com/ict/backend/service/UserService.java
@@ -0,0 +1,4 @@
+package com.ict.backend.service;
+
+public interface UserService {
+}
