e0b2876f31d052063664125441110fd8b1390b0c
kimbj829 <49744538+kimbj829@users.noreply.github.com>
2024-09-24 12:48:42 +0900

    누락 폴더 추가
    css, upload, img, pages
    -pages에 mypage, list, admin 폴더 생성
    각 페이지에 맞게 css 및 내용 수정해서 사용가능

---
+++ frontend/src/pages/admin/Home.js
@@ -0,0 +1,14 @@
+import react, { useState } from 'react';
+import '../App.css';
+import axios from 'axios';
+function Home() {
+
+  return (
+      <div>
+        <p>HOME</p>
+      </div>
+    
+  );
+}
+
+export default Home;
