367c093d9bf11a50a236cba95c1659b07b912ded
Ahnsowww <sainte12@naver.com>
2024-09-24 17:51:38 +0900

    혜린_240924
    - react-scripts 설치
    - MovieCategories.js / MovieCategories.css 작업(프론트)
    - MovieList.js / MovieList.css 작업(프론트)
    - App.js에 작업한 내용 라우터 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import './App.css';
 
 import Home from './pages/Home'
 import Layout from './pages/Layout'
 import MovieCategories from './pages/movies/MovieCategories'
+import MovieList from './pages/movies/MovieList';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
             <Route path="/" element={<Layout/>}>
                 <Route path='/' element={<Home/>}></Route>
                 <Route path='/categories' element={<MovieCategories/>}></Route>
+                <Route path="/categories/:type/:id" element={<MovieList/>} /> {/* 동적 라우트 */}
             </Route>
         </Routes>
     </BrowserRouter>
