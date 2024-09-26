7d57ff27d2f0f6c322ee1bab9a27789b98ccc972
kimbj829 <49744538+kimbj829@users.noreply.github.com>
2024-09-26 17:00:58 +0900

    김병조 (2024.09.26) login,mypage추가
    loginpage, mypage 추가

---
+++ frontend/src/pages/mypage/LoginPage.js
@@ -0,0 +1,16 @@
+import react, { useState } from 'react';
+//import '../App.css';
+import axios from 'axios';
+function LoginPage() {
+
+  return (
+      <div class = "loginpage">
+        <div class = "container">
+          <p>test</p>
+        </div>
+      </div>
+    
+  );
+}
+
+export default LoginPage;
