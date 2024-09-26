573ff9ff21849c4cb7decf8e7380d8d6dfec84e6
kimbj829 <49744538+kimbj829@users.noreply.github.com>
2024-09-26 18:27:22 +0900

    김병조(2024.09.26) 로그인 수정
    18:27
    Front 로그인 페이지 수정

--- frontend/src/pages/mypage/LoginPage.js
+++ frontend/src/pages/mypage/LoginPage.js
@@ -1,12 +1,22 @@
 import react, { useState } from 'react';
-//import '../App.css';
+import '../../css/mypage/loginpage.css';
 import axios from 'axios';
 function LoginPage() {
 
   return (
       <div class = "loginpage">
         <div class = "container">
-          <p>test</p>
+          <div class = "loginsession">
+            <div>
+              <span>로그인</span>
+              <a>비밀번호를 잊어버리셨나요?</a>
+            </div>
+            <form>
+              <input type = "text" id = "userid" placeholder='아이디'/>
+              <input type = "password" id = "userpwd" placeholder='비밀번호'/>
+              <button type = "submit">로그인하기</button>
+            </form>
+          </div>
         </div>
       </div>
     
