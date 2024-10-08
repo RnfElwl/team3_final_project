415cf001ff13b26b86240099209330d33a641114
kattenten <youngjw10000@naver.com>
2024-10-08 19:05:09 +0900

    관리자페이지 차트 추가
    대시보드 진행도: 1/6

--- backend/backend/src/main/java/com/ict/backend/controller/AdminController.java
+++ backend/backend/src/main/java/com/ict/backend/controller/AdminController.java
@@ -4,25 +4,39 @@ import com.ict.backend.service.AdminService;
 import com.ict.backend.vo.AdminVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/adminTest")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@RequestMapping("/admin")
+//@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class AdminController {
     @Autowired
     AdminService adminService;
 
-    @GetMapping("/dashboard")
-    public List<AdminVO> dashboard(){
-        List<AdminVO> dataList=adminService.adminDataSelectSeason();
-        log.info(dataList.toString());
-        return dataList;
+    @GetMapping("/qna_dashboard/{qna_filter}")
+    public List<AdminVO> qna_dashboard(@PathVariable String qna_filter){
+        List<AdminVO> qnaDataList=new ArrayList<>();
+
+        switch (qna_filter) {
+            case "월":
+                qnaDataList = adminService.qnaDataSelectMonth();
+                break;
+            case "년":
+                qnaDataList = adminService.qnaDataSelectYear();
+                break;
+            case "일":
+                qnaDataList = adminService.qnaDataSelectDay();
+                break;
+            default:
+                log.error("Invalid qna_filter: {}", qna_filter);
+                // Optional: 예외를 던지거나 빈 리스트를 반환
+                return new ArrayList<>();
+        }
+        log.info(qnaDataList.toString());
+        return qnaDataList;
     }
 }
