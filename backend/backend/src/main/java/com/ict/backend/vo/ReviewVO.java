173b481e19d6b0289788715df4096ca798b6140c
kattenten <youngjw10000@naver.com>
2024-10-17 16:52:41 +0900

    review VO 수정 및 커밋

--- backend/backend/src/main/java/com/ict/backend/vo/ReviewVO.java
+++ backend/backend/src/main/java/com/ict/backend/vo/ReviewVO.java
@@ -11,9 +11,5 @@ public class ReviewVO {
     private String movie_review_content;
     private int rate;
     private String movie_code;
-    private int movie_review_no;
-
-    private double avgRating; // 평균 별점 추가
-    private int reviewCount; // 리뷰 개수 추가
 
 }
